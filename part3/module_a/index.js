c57941ed84a0d5f04931d604ad0ff6b4154fdfb8
salaschen <ruowei.chen.aus@gmail.com>
2021-10-02 21:09:11 +1000

    heroku

---
+++ part3/module_a/index.js
@@ -0,0 +1,90 @@
+// const http = require('http') ; 
+const express = require('express') ; 
+const app = express() ; 
+const cors = require('cors') ; 
+
+app.use(express.json()); 
+app.use(cors()) ; 
+
+let notes = [
+    {
+        id: 1, 
+        content: "HTML is easy",
+        date: "2019-05-30",
+        important: true
+    },
+    {
+        id: 2, 
+        content: "Browser can execute only Javascript",
+        date: "2019-05-30",
+        important: false,
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP PROTOCAL",
+        date: "2019-05-30",
+        important: true,
+    },
+] ;
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello world!</h1>') ;
+}) ;
+
+// GET: return all the notes
+app.get('/api/notes', (request, response) => {
+    response.json(notes) ; 
+}) ; 
+
+// GET: get a single note specified by its id.
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id) ;
+    const note = notes.find(note => note.id === id) ;
+    if (note) {
+        response.json(note) ; 
+    }
+    else {
+        response.status(404).end() ; 
+    }
+});
+
+// DELETE: delete a note specified by its id.
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id) ; 
+    notes = notes.filter(note => note.id !== id) ;
+    response.status(204).end() ; 
+}) ; 
+
+const generateId = () => {
+    const maxId = notes.length > 0?
+        Math.max(...notes.map(n => n.id)) : 0 ; 
+    return maxId + 1 ; 
+}
+
+// POST: add a new note to the server. 
+app.post('/api/notes', (request, response) => {
+    const body = request.body ; 
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        }) ;
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    } ;
+
+    // notes = notes.concat(note) ;
+    notes.push(note) ; 
+   
+    response.json(note) ; 
+}) ; 
+
+const PORT = process.env.PORT || 3001 ;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`) ; 
+}) ; 
